a1faad61ad56b794874b8459820622839c4916a2
KudoLayton <lhslhg@gmail.com>
2024-03-17 16:47:42 +0900

    fix: remove unusable codes

--- src/graphic_utils.rs
+++ src/graphic_utils.rs
@@ -1,30 +1,11 @@
 use windows::{
     core::*,
-    Foundation::Numerics::*,
-    Win32::{
-        UI::HiDpi::*,
-        System::{
-            Diagnostics::Debug::*, 
-            Threading::*,
-            LibraryLoader::*,
-        },
-        Foundation::*,
-        UI::{
-            WindowsAndMessaging::*,
-            Input::KeyboardAndMouse::*,
-            Accessibility::*,
-        },
-        Graphics::{
-            Dwm::*,
-            Gdi::*,
-            Direct2D::{*, Common::{D2D1_ALPHA_MODE_PREMULTIPLIED, D2D1_COLOR_F}},
-            Dxgi::Common::*,
-            DirectWrite::*,
-        }
+    Win32::Graphics::{
+        Direct2D::*,
+        DirectWrite::*,
     }
-};
 
-use super::Rect;
+};
 
 pub unsafe fn implement_draw_text_box(
     text: &[u16],
@@ -61,7 +42,6 @@ pub unsafe fn implement_draw_text_box(
     };
     render_target_ref.FillRectangle(&rect, box_brush);
     render_target_ref.DrawTextLayout(
-        //Common::D2D_POINT_2F{x: rect.left, y: (max_height / 2.0) - (&text_metrics.width / 2.0)}, 
         Common::D2D_POINT_2F{x: rect.left, y: 0.0}, 
         &text_layout, 
         text_brush, 
